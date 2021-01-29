674e04d6d85be4bb69fae788dd6440108983532b
Andre Torres <andre855@gmail.com>
2021-01-29 17:12:27 +0000

    Refactor parseHeaders

--- src/request/parseHeaders.ts
+++ src/request/parseHeaders.ts
@@ -1,17 +1,26 @@
 import { Headers } from "../Http";
 
-export function parseHeaders(requestHeaders: string[]): Headers {
-  const parseHeader = (header: string): { key: string; value: string } => {
-    const delimiterIndex = header.indexOf(":");
-    const key = header.substring(0, delimiterIndex);
-    const value = header.substring(delimiterIndex + 1);
-    return { key: key, value: value.trim() };
-  };
+interface Header {
+  key: string;
+  value: string;
+}
 
+export function parseHeaders(requestHeaders: string[]): Headers {
   let headers: Headers = {};
   requestHeaders
-    .filter((header) => !header.startsWith("Cookie"))
+    .filter(filterHeaders)
     .map(parseHeader)
     .forEach((header) => (headers[header.key] = header.value));
   return headers;
 }
+
+const parseHeader: (header: string) => Header = (header) => {
+  const delimiterIndex = header.indexOf(":");
+  const key = header.substring(0, delimiterIndex);
+  const value = header.substring(delimiterIndex + 1);
+  return { key: key, value: value.trim() };
+};
+
+const filterHeaders: (header: string) => boolean = (header) => {
+  return !header.startsWith("Cookie") && header.trim() !== "";
+};
