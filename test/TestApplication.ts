e2dbb1ea31944a4de331e39ce09b4cb28af025ed
Andre Torres <andre855@gmail.com>
2021-02-16 13:55:13 +0000

    Fix broken tests

--- test/TestApplication.ts
+++ test/TestApplication.ts
@@ -16,34 +16,25 @@ export class TestApplication {
     if (request.uri === "/success") {
       return new HttpResponse(200, "Request Successful");
     } else if (request.uri.startsWith("/query")) {
-      return this.handleQuery(request);
+      const response = JSON.stringify({
+        id: request.params.get("id"),
+        test: request.params.get("test"),
+      });
+      return new HttpResponse(200, response);
     } else if (request.uri === "/post") {
       return new HttpResponse(200, request.body);
     } else if (request.uri === "/post/form") {
-      return this.handlePostForm(request);
+      const responseBody: { [x: string]: string } = {};
+      if (request.formData !== undefined) {
+        for (const data of request.formData) {
+          if (data[0] !== "id") {
+            responseBody[data[0]] = data[1];
+          }
+        }
+      }
+      return new HttpResponse(201, JSON.stringify(responseBody));
     } else {
       throw new InvalidRequestError();
     }
-  }
-
-  private handlePostForm(request: HttpRequest) {
-    const responseBody: { [x: string]: string } = {};
-    if (request.formData !== undefined) {
-      for (const data of request.formData) {
-        if (data[0] !== "id") {
-          responseBody[data[0]] = data[1];
-        }
-      }
-    }
-    return new HttpResponse(201, JSON.stringify(responseBody));
-  }
-
-  private handleQuery(request: HttpRequest) {
-    const response = JSON.stringify({
-      id: request.params.get("id"),
-      test: request.params.get("test"),
-    });
-    JSON.parse(response);
-    return new HttpResponse(200, response);
   }
 }
